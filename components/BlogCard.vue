4cad9264e2b523ab8bcbb3501f95f79405be49e1
arpado <ozsvar.arpad@gmail.com>
2023-06-26 18:48:45 +0200

    style and layout modified

--- components/BlogCard.vue
+++ components/BlogCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="blog-card">
+  <div class="blog-card border-8 border-solid border-white rounded">
     <img
       class="article-img w-full object-cover"
       :style="{ 'object-position': content.imgPos }"
@@ -8,23 +8,12 @@
     />
 
     <div class="blog-text m-8 space-y-4">
-      <NuxtLink class="block text-xl" :to="content.link">
+      <NuxtLink class="block text-center text-2xl text-blue-500" :to="content.link">
         <!-- {{ content.title }} -->
         {{ data.title }}
       </NuxtLink>
 
-      <ContentRendererMarkdown
-        v-if="data"
-        :value="data"
-        :excerpt="true"
-        class="textbox"
-      />
-      <NuxtLink class="block" :to="content.link">
-        <!-- {{ content.title }} -->
-        READ MORE
-      </NuxtLink>
-
-      <div class="article-details flex justify-between">
+      <div class="article-details flex justify-between py-1">
         <div class="author flex items-center">
           <ProfilePic imgsrc="images/20.jpg" />
           <span class="mx-2">{{ data.author }}</span>
@@ -33,6 +22,18 @@
           new Date(data.created).toLocaleString()
         }}</span>
       </div>
+
+      <ContentRendererMarkdown
+        v-if="data"
+        :value="data"
+        :excerpt="true"
+        class="textbox"
+      />
+      <NuxtLink class="block underline underline-offset-4 decoration-2 text-blue-500" :to="content.link">
+        <!-- {{ content.title }} -->
+        READ MORE
+      </NuxtLink>
+
     </div>
   </div>
 </template>
@@ -72,11 +73,11 @@ export default {
   min-width: 200px;
   width: 100%;
   max-width: 400px;
+  box-shadow: 2px 2px 10px 0 black;
 }
 .article-img {
   height: 250px;
 }
 .article-details {
-  margin-top: 4rem;
 }
 </style>
