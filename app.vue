6a5a7d0f898c1dfd420e4fb2eb767a3d4d8ab8ea
arpado <ozsvar.arpad@gmail.com>
2023-07-05 15:53:24 +0200

    page transitions added

--- app.vue
+++ app.vue
@@ -66,4 +66,13 @@ const links = [
   height: 0.2rem;
   background: royalblue;
 }
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
 </style>
